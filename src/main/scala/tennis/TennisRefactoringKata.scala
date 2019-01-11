af3189a2a60ca9177c90982ec5633fb0fb8d9d14
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-01-11 14:30:28 +0000

    Changes from 11th

--- src/main/scala/tennis/TennisRefactoringKata.scala
+++ src/main/scala/tennis/TennisRefactoringKata.scala
@@ -1,132 +1,151 @@
 package tennis
 
 trait TennisGame {
-  def wonPoint(x : String )
-  def calculateScore() : String
+  def wonPoint(x: String)
+
+  def calculateScore(): String
 }
 
 
-class TennisGameImplementation(val player1Name : String, val player2Name : String) extends TennisGame {
+class TennisGameImplementation(val player1Name: String, val player2Name: String) extends TennisGame {
 
   var P1point = 0
-  var P2point  = 0
+  var P2point = 0
 
   var P1res = ""
   var P2res = ""
 
-  def calculateScore() : String = {
+  def calculateScore(): String = {
     var score = ""
-    if (P1point == P2point && P1point < 4)
-    {
-      if (P1point==0)
-        score = "Love"
-      if (P1point==1)
-        score = "Fifteen"
-      if (P1point==2)
-        score = "Thirty"
-      score += "-All"
+
+    if (playersAreDrawing) {
+      score = P1point match {
+        case 0 => "Love-All"
+        case 1 => "Fifteen-All"
+        case 2 => "Thirty-All"
+        case _ => "Deuce"
+
+      }
     }
-    if (P1point==P2point && P1point>=3)
-      score = "Deuce"
 
-    if (P1point > 0 && P2point==0)
-    {
-      if (P1point==1)
-        P1res = "Fifteen"
-      if (P1point==2)
-        P1res = "Thirty"
-      if (P1point==3)
-        P1res = "Forty"
+    def processScoreIfOnePlayerIsZero(point: Int): String ={
+      point match {
+        case 1 => "Fifteen"
+        case 2 => "Thirty"
+        case 3 => "Forty"
+        case _ => ""
+      }
+    }
 
+
+    if (onlyPlayerTwoIsZero) {
+      P1res = processScoreIfOnePlayerIsZero(P1point)
       P2res = "Love"
       score = P1res + "-" + P2res
     }
-    if (P2point > 0 && P1point==0)
-    {
-      if (P2point==1)
-        P2res = "Fifteen"
-      if (P2point==2)
-        P2res = "Thirty"
-      if (P2point==3)
-        P2res = "Forty"
 
+    if (onlyPlayerOneIsZero) {
+      P2res = processScoreIfOnePlayerIsZero(P2point)
       P1res = "Love"
       score = P1res + "-" + P2res
     }
 
-    if (P1point>P2point && P1point < 4)
-    {
-      if (P1point==2)
-        P1res="Thirty"
-      if (P1point==3)
-        P1res="Forty"
-      if (P2point==1)
-        P2res="Fifteen"
-      if (P2point==2)
-        P2res="Thirty"
-      score = P1res + "-" + P2res
-    }
-    if (P2point>P1point && P2point < 4)
-    {
-      if (P2point==2)
-        P2res="Thirty"
-      if (P2point==3)
-        P2res="Forty"
-      if (P1point==1)
-        P1res="Fifteen"
-      if (P1point==2)
-        P1res="Thirty"
+    if (playerOneIsLeading) {
+      P1res = scoreIf30or40(P1point).getOrElse(P1res)
+
+      P2res = scoreIf15or30(P2point).getOrElse(P2res)
       score = P1res + "-" + P2res
     }
 
-    if (P1point > P2point && P2point >= 3)
-    {
-      score = "Advantage player1"
+    if (playerTwoIsLeading) {
+      P2res = scoreIf30or40(P2point).getOrElse(P2res)
+      P1res = scoreIf15or30(P1point).getOrElse(P1res)
+      score = P1res + "-" + P2res
     }
 
-    if (P2point > P1point && P1point >= 3)
-    {
-      score = "Advantage player2"
-    }
+    score = outcome(P1point, P2point, "player1").getOrElse(score)
+    score = outcome(P2point, P1point, "player2").getOrElse(score)
 
-    if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-    {
-      score = "Win for player1"
-    }
-    if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-    {
-      score = "Win for player2"
-    }
     return score
   }
 
-  def SetP1Score(number : Int) {
+  private def playerTwoIsLeading = {
+    P2point > P1point && P2point < 4
+  }
 
-    for (i <- 0 until number by 1)
-    {
+  private def playerOneIsLeading = {
+    P1point > P2point && P1point < 4
+  }
+
+  private def onlyPlayerOneIsZero = {
+    P2point > 0 && P1point == 0
+  }
+
+  private def onlyPlayerTwoIsZero = {
+    P1point > 0 && P2point == 0
+  }
+
+  private def playersAreDrawing = {
+    P1point == P2point
+  }
+
+
+
+  def scoreIf30or40(point1: Int): Option[String] = {
+    point1 match {
+      case 2 => Some("Thirty")
+      case 3 => Some("Forty")
+      case _ => None
+    }
+  }
+
+  def scoreIf15or30(point1: Int): Option[String] = {
+    point1 match {
+      case 1 => Some("Fifteen")
+      case 2 => Some("Thirty")
+      case _ => None
+    }
+  }
+
+  private def outcome(point1: Int, point2:Int, playerAdv: String): Option[String] = {
+
+    if (point1 >= 4 && point2 >= 0 && (point1 - point2) >= 2) {
+      Some(s"Win for $playerAdv")
+    }
+    else if (point1 > point2 && point2 >= 3) {
+      Some(s"Advantage $playerAdv")
+    }
+    else {
+      None
+    }
+  }
+
+
+  def SetP1Score(number: Int) {
+
+    for (i <- 0 until number by 1) {
       P1Score()
     }
 
   }
 
-  def SetP2Score(number : Int){
+  def SetP2Score(number: Int) {
 
-    for (i <- 0 until number by 1)
-    {
+    for (i <- 0 until number by 1) {
       P2Score()
     }
 
   }
 
-  def P1Score(){
+  def P1Score() {
     P1point += 1
   }
 
-  def P2Score(){
-    P2point +=1
+  def P2Score() {
+    P2point += 1
   }
 
-  def wonPoint(player : String) {
+  def wonPoint(player: String) {
     if (player == "player1")
       P1Score()
     else
